cf83eab057f538ea7f8a9906461f3ce428cee087
Cirilli, Ignacio <88701429+IgnacioCirilli@users.noreply.github.com>
2023-05-20 16:33:41 -0300

    nueva prueba

--- test/ar/edu/unq/po2/prueba/PuntoTestCase.java
+++ test/ar/edu/unq/po2/prueba/PuntoTestCase.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.po2.prueba;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,25 @@ import org.junit.jupiter.api.Test;
 class PuntoTestCase {
 
 	private Punto punto1;
+	private Punto punto2;
 	
 	@BeforeEach
 	void setUp() {
 		punto1 = new Punto(2,4);
+		punto2 = mock(Punto.class);
+		
 	}
 	
 	@Test
 	void testPrueba() {
 		assertEquals(2,punto1.getX());
+	}
+	
+	@Test
+	void testPrueba1() {
+		when(punto2.getX()).thenReturn(2f);
+	
+		assertEquals(2f, punto2.getX());
 	}
 
 }
